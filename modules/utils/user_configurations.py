b770ca27a5b3147385dff32f6ed6531b613cccec
FilTheo <theodosiou.filotas@gmail.com>
2024-11-21 10:21:37 +0200

    new model configurations

--- modules/utils/user_configurations.py
+++ modules/utils/user_configurations.py
@@ -1,4 +1,12 @@
 PASSWORDS = {
     "m5_password": "m5",
-    "client_1_password": "client_1"
+    "client_1_password": "client_1",
+    "m5_test_1_password": "m5_test_1",
+    "m5_test_2_password": "m5_test_2",
+    "m5_test_3_password": "m5_test_3",
+    "m5_test_4_password": "m5_test_4",
+    "m5_test_5_password": "m5_test_5",
+    "m5_test_6_password": "m5_test_6",
+    "m5_test_7_password": "m5_test_7",
+
 }
